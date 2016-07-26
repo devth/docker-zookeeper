996083e646147869e4dcfd6b7ada3d4e2b19c278
Trevor Hartman <trevorhartman@gmail.com>
2016-07-26 06:34:47 -0600

    Add optional SSL configuration via ENV vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,5 +13,34 @@ if [ -n "$ZOO_CFG" ]; then
 	echo $ZOO_CFG >> /zookeeper/conf/zoo.cfg
 fi
 
+#
+# Optional SSL configuration
+#
+
+if [ -n "$SSL_KEYSTORE_LOCATION" ]; then
+	echo "ssl.keyStore.location=$SSL_KEYSTORE_LOCATION" >> /zookeeper/conf/zoo.cfg
+fi
+
+if [ -n "$SSL_KEYSTORE_PASSWORD" ]; then
+	echo "ssl.keyStore.password=$SSL_KEYSTORE_PASSWORD" >> /zookeeper/conf/zoo.cfg
+fi
+
+if [ -n "$SSL_TRUSTSTORE_LOCATION" ]; then
+	echo "ssl.trustStore.location=$SSL_TRUSTSTORE_LOCATION" >> /zookeeper/conf/zoo.cfg
+fi
+
+if [ -n "$SSL_TRUSTSTORE_PASSWORD" ]; then
+	echo "ssl.trustStore.password=$SSL_TRUSTSTORE_PASSWORD" >> /zookeeper/conf/zoo.cfg
+fi
+
+#
+# Other optional configuration from ZOO_CFG env var
+#
+
+if [ -n "$ZOO_CFG" ]; then
+	echo >> /zookeeper/conf/zoo.cfg
+	echo $ZOO_CFG >> /zookeeper/conf/zoo.cfg
+fi
+
 cd /zookeeper
 exec "$@"
